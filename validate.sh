feb2b2ac44bd9514337f078714bdfcef42dd6dd6
Gina Häußge <gina@foosel.net>
2026-03-20 14:51:59 +0100

    feat: even more validating

--- validate.sh
+++ validate.sh
@@ -8,15 +8,25 @@ BASE="public"
 pushd $BASE
 
   # Validate json files
+  echo "Validating json files"
   for json in $(find . -iname "*.json" -type f); do
-    echo "Validating $json..."
+    echo "$json..."
     jq . "$json" 1> /dev/null
   done
 
   if command -v check-jsonschema >/dev/null 2>&1; then
     echo
-    echo "Validating human.json against JSON schema..."
+
+    echo "Validating json files against known JSON schemata..."
+
+    echo "./human.json..."
     check-jsonschema --schemafile https://codeberg.org/robida/human.json/raw/branch/main/schema/0.1.1.json human.json
+
+    # jsonfeed
+    for feed in $(find . -iname "feed.json" -type f); do
+        echo "$feed..."
+        check-jsonschema --schemafile https://raw.githubusercontent.com/sonicdoe/jsonfeed-schema/refs/heads/master/schema-v1.1.json "$feed"
+    done
   fi
 
 popd
